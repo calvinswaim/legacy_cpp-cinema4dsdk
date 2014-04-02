f81cb5e89d984018c0701efe6f7e1c94db6ac24b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2014-04-02 02:15:15 +0200

    updated file header copyright

--- src/starters/commands/spheres-on-points.cpp
+++ src/starters/commands/spheres-on-points.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013, Niklas Rosenstein (rosensteinniklas@gmail.com)
+ * Copyright (c) 2013-2014  Niklas Rosenstein
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
